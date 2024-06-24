ee2b9cb11898696602517e0c739911c5a39fd1eb
joyeajin <dpwls31200@gmail.com>
2024-06-24 17:18:41 +0900

    객체지향 : class

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
-function Machine(name, age) {
-  this.name = name;
-  this.age = age;
-  this.sayHi = `안녕하세요 ${name}입니다.`;
+class Parent {
+  constructor(name) {
+    this.name = name;
+    // this.sayHi = function () {
+    //   console.log("hi");
+    // };
+  }
+  sayHi() {
+    console.log("hi");
+  }
+  sayHello() {
+    console.log("hello");
+  }
 }
 
-Machine.prototype.gender = "male";
-
-let student1 = new Machine("kim", 15);
-let student2 = new Machine("jo", 14);
-
-student1.toString();
-
-function Product(name, price) {
-  this.name = name;
-  this.price = price;
-  this.부가세 = function () {
-    console.log(this.price * 0.1);
-  };
-}
-
-let product1 = new Product("shirts", 50000);
-
-let parent = { name: "kim" };
-let child = {};
-child.__proto__ = parent;
+let child = new Parent("jo");
+console.log(child.__proto__);
+console.log(Object.getPrototypeOf(child));

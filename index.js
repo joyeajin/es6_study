0bbd054b48547cc203711a1d83adcc10d9c88fc7
joyeajin <dpwls31200@gmail.com>
2024-08-02 16:55:17 +0900

    getter / setter

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//---------------class-------------------
 class Parent {
   constructor(name) {
     this.name = name;
@@ -14,5 +15,37 @@ class Parent {
 }
 
 let child = new Parent("jo");
-console.log(child.__proto__);
-console.log(Object.getPrototypeOf(child));
+// console.log(child.__proto__);
+// console.log(Object.getPrototypeOf(child));
+
+//-----------------extends / super-------------------
+class GrandFather {
+  constructor(name) {
+    this.성 = "kim";
+    this.이름 = name;
+  }
+}
+let grandFather1 = new GrandFather("hoho");
+
+class Father extends GrandFather {
+  constructor(name) {
+    super(name);
+    this.age = 50;
+  }
+}
+let father1 = new Father("lili");
+
+//-----------------getter / setter-------------------
+let person = {
+  name: "Park",
+  age: 30,
+  get nextAge() {
+    return this.age + 1;
+  },
+  set setAge(age) {
+    this.age = parseInt(age);
+  },
+};
+
+console.log((person.setAge = "20"));
+console.log(person);
